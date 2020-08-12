5d495e63ee787c2515ec9823cc61006daeef238a
362569221@qq.com <362569221@qq.com>
2020-08-12 22:59:45 +0800

    郭毅 修改灾害点行政区划

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
       <div id="trackPopUpContent" class="leaflet-popup" style="top:5px;left:0;z-index:1;">
         <a class="leaflet-popup-close-button" href="#">×</a>
         <div class="leaflet-popup-content-wrapper">
-          <div id="trackPopUpLink" class="leaflet-popup-content" style="max-width: 300px;"></div>
+          <div id="trackPopUpLink" class="leaflet-popup-content" style="max-width: 350px;"></div>
         </div>
         <div class="leaflet-popup-tip-container">
           <div class="leaflet-popup-tip"></div>
@@ -230,7 +230,7 @@ export default {
     var monitorEntity = that.viewer.entities.add({
       name: "video",
       // eslint-disable-next-line no-undef
-      position: Cesium.Cartesian3.fromDegrees(109.44, 32.11, 30.0),
+      position: Cesium.Cartesian3.fromDegrees(109.44, 32.11, 0),
       point: {
         //点
         pixelSize: 0,
@@ -255,7 +255,7 @@ export default {
         backgroundColor: new Cesium.Color(0.5, 0.6, 1, 1.0),
         disableDepthTestDistance: Number.POSITIVE_INFINITY,
         // eslint-disable-next-line no-undef
-        heightReference:Cesium.HeightReference.CLAMP_TO_GROUND
+        // heightReference:Cesium.HeightReference.CLAMP_TO_GROUND
       },
       billboard: {
         //图标
@@ -265,7 +265,7 @@ export default {
         // eslint-disable-next-line no-undef
         disableDepthTestDistance: Number.POSITIVE_INFINITY,
         // eslint-disable-next-line no-undef
-        heightReference:Cesium.HeightReference.CLAMP_TO_GROUND
+        // heightReference:Cesium.HeightReference.CLAMP_TO_GROUND
       },
     });
     // 视角定位
@@ -295,51 +295,53 @@ export default {
     //   // eslint-disable-next-line no-undef
     // }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
 
-    handler3D.setInputAction(function (movement) {
-      var pick = that.viewer.scene.pick(movement.position);
-      if (pick && pick.id) {
-        console.log(pick.id);
+    // ScreenSpaceEventHandler   处理用户输入事件。可以添加自定义功能以在以下位置执行当用户输入输入时。
+    handler3D.setInputAction(function(movement) {
+        // 监听鼠标的当前位置坐标，然后根据当前坐标去动态更新气泡窗口div的显示位置；
+        var pick = that.viewer.scene.pick(movement.position);
+        // ovement.position是获取屏幕坐标
+        // pick 记录当前屏幕位置
+        if(pick && pick){
+            // 这里的判断条件还是蛮有用的,比如你点击某些点的时候想弹出自定义弹窗,其他点弹出原生弹窗,就需要在这里进行判断了
+            // eslint-disable-next-line no-undef
+            $("#trackPopUp").show();
+            // 显示弹窗容器
+            // eslint-disable-next-line no-undef
+            var cartographic = Cesium.Cartographic.fromCartesian(pick.primitive._actualPosition);
+            // 获取点的经纬度
+            var point=[cartographic.longitude / Math.PI * 180, cartographic.latitude / Math.PI * 180];
+            // 转换坐标
+            // eslint-disable-next-line no-undef
+            var destination=Cesium.Cartesian3.fromDegrees(point[0], point[1], 3000.0);
+            // destination是我们点击之后,flyto的位置
+            let content =
+                '<table  border="1" width="400px" colspan="4">'+
+                '    <thead ><th colspan="4" style="text-align: center">七里河区第二号地质灾害点</th></thead>'+
+                '    <tbody>'+
+                '    <tr >'+
+                '        <th align="left">地理位置:甘肃省兰州市起立河区<br/>管道桩号:7578323N<br/>灾害类型:小型<br/>风险等级:严重</th>'+
+                '        <th align="center"><img src="' + '凯尔特人.jpg' + ' " alt="灾害点图片加载失败" width="100px" height="100px"></th>'+
+                '    </tr>'+
+                '    </tbody>'+
+                '    <tfoot >'+
+                '    <td colspan="4" align="center">'+
+                '        <button style="color: #003da8">基本特征</button>'+
+                '        <button style="color: #003da8" οnclick="this">勘察设计</button>'+
+                '        <button style="color: #003da8">巡检卡</button>'+
+                '        <button style="color: #003da8">历年对比</button>'+
+                '        <button style="color: #003da8">巡查上报</button></td>'+
+                '    </tfoot>'+
+                '</table>';
+            // content是核心,你想弹出的东西,就全部放在这里面
+            var obj = {position:movement.position,destination:destination,content:content};
+            // 构造一个参数,包括事件、 位置、已经弹框
+            that.infoWindow(obj,pick);
+        }
+        else{
+          // eslint-disable-next-line no-undef
+          $('#trackPopUp').hide();
+        }
         // eslint-disable-next-line no-undef
-        $("#trackPopUp").show();
-        // eslint-disable-next-line no-undef
-        var cartographic = Cesium.Cartographic.fromCartesian(movement.position);
-        var point = [
-          (cartographic.longitude / Math.PI) * 180,
-          (cartographic.latitude / Math.PI) * 180,
-        ];
-        // eslint-disable-next-line no-undef
-        var destination = Cesium.Cartesian3.fromDegrees(
-          point[0],
-          point[1],
-          3000.0
-        );
-        var id = pick.id._id.replace(/[^0-9]/gi, "");
-        var content =
-          '<table><tbody><tr><th style="color:black;">' +
-          pick.id._name +
-          '</th><td><button style="color:black;" οnclick="updateValve(' +
-          id +
-          ')">确定</button></td><td><button οnclick="deleteValve(' +
-          id +
-          ')" style="color:black;">删除</button></td></tr>' +
-          '<tr><th style="color:black;">类型</th><td><input style="color:black;" value=111></td></tr>' +
-          '<tr><th style="color:black;">经度</th><td><input id="x" style="color:black;" value=222></td></tr>' +
-          '<tr><th style="color:black;">纬度</th><td><input id="y" style="color:black;" value=333></td></tr>' +
-          '<tr><th style="color:black;">类型</th><td><input style="color:black;" value=111></td></tr>' +
-          '<tr><th style="color:black;">经度</th><td><input id="x" style="color:black;" value=222></td></tr>' +
-          '<tr><th style="color:black;">纬度</th><td><input id="y" style="color:black;" value=333></td></tr>' +
-          "</tbody></table>";
-        var obj = {
-          position: movement.position,
-          destination: destination,
-          content: content,
-        };
-        that.infoWindow(obj);
-      } else {
-        // eslint-disable-next-line no-undef
-        $("#trackPopUp").hide();
-      }
-      // eslint-disable-next-line no-undef
     }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
   },
   data() {
@@ -348,91 +350,112 @@ export default {
     };
   },
   methods:{
-    infoWindow(obj) {
+    infoWindow(obj,pick) {
       let that = this
-      var picked = that.viewer.scene.pick(obj.position);
-      // eslint-disable-next-line no-undef
-      if (Cesium.defined(picked)) {
+        var picked = that.viewer.scene.pick(obj.position);
+        // 首先获取点击点的信息
         // eslint-disable-next-line no-undef
-        var id = Cesium.defaultValue(picked.id, picked.primitive.id);
-        // eslint-disable-next-line no-undef
-        if (id instanceof Cesium.Entity) {
-          // eslint-disable-next-line no-undef
-          $(".cesium-selection-wrapper").show();
-          // eslint-disable-next-line no-undef
-          $("#trackPopUpLink").empty();
-          // eslint-disable-next-line no-undef
-          $("#trackPopUpLink").append(obj.content);
-          // eslint-disable-next-line no-inner-declarations
-          function positionPopUp(c) {
+        if (Cesium.defined(picked)) {
+            // 判断 如果点被定义了
             // eslint-disable-next-line no-undef
-            var x = c.x - $("#trackPopUpContent").width() / 2;
-            // eslint-disable-next-line no-undef
-            var y = c.y - $("#trackPopUpContent").height();
-            // eslint-disable-next-line no-undef
-            $("#trackPopUpContent").css(
-              "transform",
-              "translate3d(" + x + "px, " + y + "px, 0)"
-            );
-          }
-          // eslint-disable-next-line no-undef
-          var c = new Cesium.Cartesian2(obj.position.x, obj.position.y);
-          // eslint-disable-next-line no-undef
-          $("#trackPopUp").show();
-          positionPopUp(c); // Initial position
-          // at the place item
-          // picked
-          let changedC = null;
-          let removeHandler = that.viewer.scene.postRender.addEventListener(
-            function () {
-              if (picked.id._polyline != null) {
-                var pos = {};
-                pos.x =
-                  (id._polyline._positions._value["0"].x +
-                    id._polyline._positions._value[1].x) /
-                  2;
-                pos.y =
-                  (id._polyline._positions._value["0"].y +
-                    id._polyline._positions._value[1].y) /
-                  2;
-                pos.z =
-                  (id._polyline._positions._value["0"].z +
-                    id._polyline._positions._value[1].z) /
-                  2;
+            var id = Cesium.defaultValue(picked.id, picked.primitive.id); 
+            // 获取id(id就是原生弹窗的标题)
+            if(id ){
+                let back_position = null
+                if (obj.destination) {
+                  // eslint-disable-next-line no-undef
+                    back_position = new Cesium.Cartesian3.fromDegrees(that.getCenterPosition().x, that.getCenterPosition().y, that.getCenterPosition().z);
+                    // 我在这里用back_position记录的点击之前的位置,便于×掉弹窗后返回
+                    that.viewer.camera.flyTo({
+                        // 跳转到我们刚才定义的位置
+                        destination: obj.destination
+                    });
+                }
+                // 填充内容
                 // eslint-disable-next-line no-undef
-                changedC = Cesium.SceneTransforms.wgs84ToWindowCoordinates(
-                  that.viewer.scene,
-                  pos
-                );
-              } else {
+                $(".cesium-selection-wrapper").show();
+                // cesium-selection-wrapper是cesium内置的东西
                 // eslint-disable-next-line no-undef
-                changedC = Cesium.SceneTransforms.wgs84ToWindowCoordinates(
-                  that.viewer.scene,
-                  id._position._value
-                );
-              } // If things moved, move the
-              // popUp too
-              if (c.x !== changedC.x || c.y !== changedC.y) {
-                positionPopUp(changedC);
-                c = changedC;
-              }
+                $('#trackPopUpLink').empty();   
+                // empty() 方法从被选元素移除所有内容，包括所有文本和子节点。
+                // eslint-disable-next-line no-undef
+                $('#trackPopUpLink').append(obj.content);  
+                // append() 方法在被选元素的结尾（仍然在内部）插入指定内容。
+                // eslint-disable-next-line no-undef
+                var c = new Cesium.Cartesian2(obj.position.x, obj.position.y);
+                // eslint-disable-next-line no-undef
+                $('#trackPopUp').show();
+                that.positionPopUp(c); 
+                //实时更新位置
+                var removeHandler = that.viewer.scene.postRender.addEventListener(function () {
+                  // eslint-disable-next-line no-undef
+                    let changedC = Cesium.SceneTransforms.wgs84ToWindowCoordinates(that.viewer.scene, pick.primitive._actualPosition);
+                    // 我们转动地球,也会实时更新弹窗的位置.并不会一成不变
+                    if (c && changedC && c.x && changedC.x && c.y && changedC.y) {
+                        if ((c.x !== changedC.x) || (c.y !== changedC.y)) {
+                            that.positionPopUp(changedC);
+                            c = changedC;
+                        }
+                    }
+                });
+                // PopUp close button event handler
+                // eslint-disable-next-line no-undef
+                $('.leaflet-popup-close-button').click(function () {
+                  // eslint-disable-next-line no-undef
+                    $('#trackPopUp').hide();
+                    // eslint-disable-next-line no-undef
+                    $('#trackPopUpLink').empty();
+                    // eslint-disable-next-line no-undef
+                    $(".cesium-selection-wrapper").hide();
+                    removeHandler.call();
+                    if(back_position){
+                      that.viewer.camera.flyTo({
+                        // 回到我们点击前的位置
+                          destination: back_position
+                      });
+                    }
+                    
+                    return false;
+                });
+
             }
-          );
-          // PopUp close button event handler
-          // eslint-disable-next-line no-undef
-          $(".leaflet-popup-close-button").click(function () {
-            // eslint-disable-next-line no-undef
-            $("#trackPopUp").hide();
-            // eslint-disable-next-line no-undef
-            $("#trackPopUpLink").empty();
-            // eslint-disable-next-line no-undef
-            $(".cesium-selection-wrapper").hide();
-            removeHandler.call();
-            return false;
-          });
-          return id;
         }
-      }
+    },
+    getHeight(){
+      let that = this
+      //获取当前高度
+        if (that.viewer) {
+            var scene = that.viewer.scene;
+            var ellipsoid = scene.globe.ellipsoid;
+            var height = ellipsoid.cartesianToCartographic(that.viewer.camera.position).height;
+            return height;
+        }
+    },
+    getCenterPosition(){
+      let that = this
+      //获取当前位置
+      // eslint-disable-next-line no-undef
+      var result = that.viewer.camera.pickEllipsoid(new Cesium.Cartesian2(that.viewer.canvas.clientWidth / 2, that.viewer.canvas
+          .clientHeight / 2));
+          // eslint-disable-next-line no-undef
+      var curPosition = Cesium.Ellipsoid.WGS84.cartesianToCartographic(result);
+      var lon = curPosition.longitude * 180 / Math.PI;
+      var lat = curPosition.latitude * 180 / Math.PI;
+      var height = that.getHeight();
+      return {
+          x: lon,
+          y: lat,
+          z: height
+      };
+    },
+    positionPopUp(c){
+      // eslint-disable-next-line no-undef
+      var x = c.x - ($('#trackPopUpContent').width()) / 2;
+      // eslint-disable-next-line no-undef
+      var y = c.y - ($('#trackPopUpContent').height());
+      // 为所有匹配元素(#trackPopUpContent)设置transform的值为 'translate3d(' + x + 'px, ' + y + 'px, 0)'
+      // eslint-disable-next-line no-undef
+      $('#trackPopUpContent').css('transform', 'translate3d(' + x + 'px, ' + y + 'px, 0)');
     }
   }
 };
@@ -449,68 +472,56 @@ export default {
 
 /*leaflet风格气泡窗口样式模板*/
 .leaflet-popup {
-  position: absolute;
-  text-align: center;
+    position: absolute;
+    text-align: center;
 }
 .leaflet-popup-close-button {
-  position: absolute;
-  top: 0;
-  right: 0;
-  padding: 4px 4px 0 0;
-  text-align: center;
-  width: 18px;
-  height: 14px;
-  font: 16px/14px Tahoma, Verdana, sans-serif;
-  color: #c3c3c3;
-  text-decoration: none;
-  font-weight: bold;
-  background: transparent;
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 4px 4px 0 0;
+    text-align: center;
+    width: 18px;
+    height: 14px;
+    font: 16px/14px Tahoma, Verdana, sans-serif;
+    color: #c3c3c3;
+    text-decoration: none;
+    font-weight: bold;
+    background: transparent;
 }
 .leaflet-popup-content-wrapper {
-  text-align: center;
-  max-height: 200px;
-  overflow-y: auto;
-  background: white;
-  box-shadow: 0 3px 14px rgba(0, 0, 0, 0.4);
-  padding: 1px;
-  text-align: left;
-  border-radius: 12px;
+    text-align: center;
+    max-height: 200px;
+    overflow-y: auto;
+    background: white;
+    box-shadow: 0 3px 14px rgba(0,0,0,0.4);
+    padding: 1px;
+    text-align: left;
+    border-radius: 12px;
 }
 .leaflet-popup-content {
-  margin: 13px 19px;
-  line-height: 1.4;
+    margin: 13px 19px;
+    line-height: 1.4;
 }
 .leaflet-popup-tip-container {
-  margin: 0 auto;
-  width: 200px;
-  height: 15px;
-  position: relative;
-  overflow: hidden;
+    /*修改弹窗位置*/
+    margin: 0 auto;
+    width: 200px;
+    height: 50px;
+    position: relative;
+    overflow: hidden;
 }
 .leaflet-popup-tip {
-  background: white;
-  box-shadow: 0 3px 14px rgba(0, 0, 0, 0.4);
-  width: 17px;
-  height: 17px;
-  padding: 1px;
-  margin: -10px auto 0;
-  -webkit-transform: rotate(45deg);
-  -moz-transform: rotate(45deg);
-  -ms-transform: rotate(45deg);
-  -o-transform: rotate(45deg);
-  transform: rotate(45deg);
-}
-
-/*按钮样式*/
-.add-button {
-}
-.noselect {
-  -webkit-touch-callout: none; /* iOS Safari */
-  -webkit-user-select: none; /* Chrome/Safari/Opera */
-  -khtml-user-select: none; /* Konqueror */
-  -moz-user-select: none; /* Firefox */
-  -ms-user-select: none; /* Internet Explorer/Edge */
-  user-select: none; /* Non-prefixed version, currently
-  not supported by any browser */
+    background: white;
+    box-shadow: 0 3px 14px rgba(0,0,0,0.4);
+    width: 17px;
+    height: 17px;
+    padding: 1px;
+    margin: -10px auto 0;
+    -webkit-transform: rotate(45deg);
+    -moz-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+    transform: rotate(45deg);
 }
 </style>
